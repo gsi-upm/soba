4a5a3a9871f3276a3e357f8458af78e4c6802a32
Merinom <eduardo.merinom13@gmail.com>
2018-04-12 11:37:41 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy', 'anaconda-client','tqdm','mesa','transitions', 'tornado', 'pandas', 'tornado', 'tornado.ioloop', 'tornado.server', 'tornado.web', 'tornado.websocket', 'tornado.escape', 'tornado.gen']
+MOCK_MODULES = ['mesa', 'numpy', 'anaconda-client','tqdm','mesa','transitions', 'tornado', 'pandas', 'tornado', 'tornado.ioloop', 'tornado.server', 'tornado.web', 'tornado.websocket', 'tornado.escape', 'tornado.gen']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
